22c8c29d912e43e8f3f11ad8a43ea2720bbac2e6
andrewadinegaraignia <38481207+andrewadinegaraignia@users.noreply.github.com>
2018-09-07 14:11:04 +0800

    Update RunScript2.ps1

--- RunScript2.ps1
+++ RunScript2.ps1
@@ -1,4 +1,7 @@
-$TestResult  = $OctopusParameters["Octopus.Action[RunScript].GeneratedForecast"]
+$TestResult  = $OctopusParameters["Octopus.Action[RunScript].Output.GeneratedForecast"]
 
 Write-Host "Accessing Output variable from previous step"
+
+Write-Host Octopus.Action[RunScript].Output.Manual.ResponsibleUser.Id
+
 Write-Host $TestResult
